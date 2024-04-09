9a84967edd3daefaaba1278fad4ee2325dc938e4
James Kelly <jk377y@gmail.com>
2024-04-09 15:31:33 -0500

    changes to header, added media query

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,16 @@ const Header = () => {
         <header>
             <div className='container'>
                 <img src={LOGO} className='logo_a' alt='logo' />
-                <h1>Sports Meet and Greet</h1>
+                <div className='title_area'>
+                    <h1>Sports Meet and Greet</h1>
+                    <nav>
+                        <ul>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">About</a></li>
+                            <li><a href="">Contact</a></li>
+                        </ul>
+                    </nav>
+                </div>
                 <img src={LOGO} className='logo_b' alt='logo' />
             </div>
         </header>
