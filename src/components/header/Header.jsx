9c4c0ff78ca360b4ea8a95dd79a4e998ce01a87a
James Kelly <jk377y@gmail.com>
2024-04-09 17:21:31 -0500

    added toggle for login links based on user being logged in

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,22 +2,35 @@ import '../../assets/index.css';
 import LOGO from '../../assets/images/logo1.png';
 import './Header.css'
 
-const Header = () => {
+const Header = ({ signedIn }) => {
+    signedIn = false; //! Remove this line after implementing the authentication
     return (
         <header>
             <div className='container'>
-                <img src={LOGO} className='logo_a' alt='logo' />
+                <a href="/" className='logo_a'>
+                    <img src={LOGO} alt='logo' />
+                </a>
                 <div className='title_area'>
                     <h1>Sports Meet and Greet</h1>
                     <nav>
                         <ul>
-                            <li><a href="">Home</a></li>
-                            <li><a href="">About</a></li>
-                            <li><a href="">Contact</a></li>
+                            <li><a href="/" className='nav_links'>Home</a></li>
+                            <li><a href="/about" className='nav_links'>About</a></li>
+                            <li><a href="/contact" className='nav_links'>Contact</a></li>
+                            
+                            {!signedIn && (
+                                <>
+                                    <li><a href="/login" className='nav_links'>Login</a></li>                                                      
+                                    <li><a href="/signup" className='nav_links'>Sign up</a></li>
+                                </>
+                            )}
+
                         </ul>
                     </nav>
                 </div>
-                <img src={LOGO} className='logo_b' alt='logo' />
+                <a href="/" className='logo_b'>
+                    <img src={LOGO} alt='logo' />
+                </a>
             </div>
         </header>
     )
