d512826484e63f0bc603f53b46458fc3d0f4d8fe
James Kelly <jk377y@gmail.com>
2024-04-10 00:14:51 -0500

    added gallery placeholder on homepage

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,6 +1,7 @@
 
 import '../../assets/index.css';
 import './Homepage.css';
+import placeholderImage from '../../assets/images/teamSports.webp';
 
 const Homepage = () => {
     return (
@@ -8,22 +9,38 @@ const Homepage = () => {
             <aside>
                 <h3 align='center'>Upcoming Events</h3>
                 <ul className='events_items'>
-                    <li>
-                        <h6>Saturday, Apr. 13th</h6>
-                        <p>soccer tourney</p>
-                    </li>
-                    <li>
-                        <h6>Monday, Apr. 15th</h6>
-                        <p>tennis match</p>
-                    </li>
-                    <li>
-                        <h6>Wednesday, Apr. 17th</h6>
-                        <p>golf tournament</p>
-                    </li>
 
-                    <li>
-                        <h6>Friday, Apr. 19th</h6>
-                        <p>swimming competition</p>
+                    <li className='events_item'>
+                        <h6 className='events_dates'>Saturday, Apr. 13th</h6>
+                        <a href="/calendar" className='events_event'>soccer tourney</a>
+                    </li>
+                    <li className='events_item'>
+                        <h6 className='events_dates'>Monday, Apr. 15th</h6>
+                        <a href="/calendar">tennis match</a>
+                    </li>
+                    <li className='events_item'>
+                        <h6 className='events_dates'>Wednesday, Apr. 17th</h6>
+                        <a href="/calendar">golf tournament</a>
+                    </li>
+                    <li className='events_item'>
+                        <h6 className='events_dates'>Friday, Apr. 19th</h6>
+                        <a href="/calendar">swimming competition</a>
+                    </li>
+                    <li className='events_item'>
+                        <h6 className='events_dates'>Saturday, Apr. 20th</h6>
+                        <a href="/calendar">soccer tourney</a>
+                    </li>
+                    <li className='events_item'>
+                        <h6 className='events_dates'>Monday, May 6th</h6>
+                        <a href="/calendar">tennis match</a>
+                    </li>
+                    <li className='events_item'>
+                        <h6 className='events_dates'>Wednesday, May 8th</h6>
+                        <a href="/calendar">golf tournament</a>
+                    </li>
+                    <li className='events_item'>
+                        <h6 className='events_dates'>Friday, May 10th</h6>
+                        <a href="/calendar">swimming competition</a>
                     </li>
 
                 </ul>
@@ -31,26 +48,13 @@ const Homepage = () => {
             <main>
                 <section>
                     <h1>Welcome to our page!</h1>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                        Sed ut odio ut sapien tincidunt fermentum. 
-                        Nullam in diam nec justo ultricies aliquet. 
-                        Sed nec est nec justo sollicitudin ultrices. 
-                        Nulla facilisi. 
-                        Nullam auctor, libero sit amet tincidunt lacinia, 
-                        turpis quam aliquam nunc, nec fermentum nunc libero et purus. 
-                        Nullam non mauris auctor, ultricies urna at, 
-                        fermentum libero. 
-                        Donec nec risus nec nunc tincidunt gravida. 
-                        Nullam in diam nec justo ultricies aliquet. 
-                        Sed nec est nec justo sollicitudin ultrices. 
-                        Nulla facilisi. 
-                        Nullam auctor, libero sit amet tincidunt lacinia, 
-                        turpis quam aliquam nunc, nec fermentum nunc libero et purus. 
-                        Nullam non mauris auctor, ultricies urna at, 
-                        fermentum libero. 
-                        Donec nec risus nec nunc tincidunt gravida.
-                    </p>
+                    
+                    <div className="image-gallery">
+                        {[...Array(20)].map((_, index) => (
+                            <img key={index} src={placeholderImage} alt={`Image ${index + 1}`} />
+                        ))}
+                    </div>
+
                 </section>
             </main>
         </div>
